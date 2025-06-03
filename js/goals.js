527d29e05acfad4a4b894eac0a4af98e7d48e12c
Drian <cfangeles0430@gmail.com>
2025-06-04 01:29:18 +0800

    mobile response homepage

--- js/goals.js
+++ js/goals.js
@@ -2,6 +2,15 @@ fetch("components/homeHeader.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("home-header").innerHTML = data;
+
+        // Now the header is loaded, we can safely attach the event listener
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navLinks.classList.toggle('active');
+    });
 });
 
 fetch("components/header.html")
@@ -10,11 +19,11 @@ fetch("components/header.html")
     document.getElementById("header").innerHTML = data;
 });
 
-  fetch("components/footer.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-  });
+fetch("components/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
 
 const cards = document.querySelectorAll('.goal-card1, .goal-card2, .goal-card3');
 
